792927bb0daf8fa3c70430fd919e7206223f21af
ktb88 <ktb88@korea.ac.kr>
2018-05-24 18:34:47 +0900

    [ethkim] update compile.js (solc issue)

--- dev/ethan/ethereum/gembling/ethereum/compile.js
+++ dev/ethan/ethereum/gembling/ethereum/compile.js
@@ -5,9 +5,17 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
-const gemPath = path.resolve(__dirname, 'contracts', 'GemFactory.sol');
-const source = fs.readFileSync(gemPath, 'utf-8');
-const output = solc.compile(source, 1).contracts;
+const contractPath = path.resolve(__dirname, 'contracts');
+var targets = {
+	"GemFactory.sol": fs.readFileSync(contractPath + '/GemFactory.sol', 'utf8'),
+	"ownership/Ownable.sol": fs.readFileSync(contractPath + '/ownership/Ownable.sol', 'utf8'),	
+	"games/Zerosum.sol": fs.readFileSync(contractPath + '/games/Zerosum.sol', 'utf8')
+};
+
+//const gemPath = path.resolve(__dirname, 'contracts', 'GemFactory.sol');
+//const source = fs.readFileSync(gemPath, 'utf-8');
+
+const output = solc.compile({sources: targets}, 1).contracts;
 
 console.log('[+] - [compile.js]');
 console.log(output);
